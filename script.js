4efe7724428daf37798feb44f6c74124e3635376
Suvarnahiremath <150912845+Suvarnahiremath@users.noreply.github.com>
2024-11-07 09:27:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// JavaScript to handle redirection
+document.getElementById('user').onclick = function() {
+    window.location.href = "user_login.html"; // Replace with the actual login page URL for User
+};
+
+document.getElementById('admin').onclick = function() {
+    window.location.href = "admin_login.html"; // Replace with the actual login page URL for Admin
+};
